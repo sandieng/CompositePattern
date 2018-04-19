42f1b1195d9c25528f9fa0d6e014b6ed452d74c4
sandi.eng <sandi.eng@fabricgroup.com.au>
2018-04-20 09:33:21 +1000

    Fixed composite iterator example

---
+++ CompositeIteratorTestClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CompositeIterator;
+
+namespace CompositeIteratorTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuComponent pancakeHouseMenu = new Menu("Pancake House Menu", "Breakfast");
+            MenuComponent dinerMenu = new Menu("Diner Menu", "Lunch");
+            MenuComponent cafeMenu = new Menu("Cafe Menu", "Dinner");
+            MenuComponent dessertMenu = new Menu("Dessert Menu", "Dessert");
+            MenuComponent allMenus = new Menu("All Menus", "All menus combined");
+
+            allMenus.Add(pancakeHouseMenu);
+            allMenus.Add(dinerMenu);
+            allMenus.Add(cafeMenu);
+
+            // Add menu items to diner menu
+            dinerMenu.Add(new MenuItem("Pasta Cucianna", "Spaghetti with rinso sauce and mixed seafood", false, 20.95));
+            dinerMenu.Add(new MenuItem("Chow Mian", "Smelly fried noodle with smelly tofu", true, 14.95));
+
+            // Add dessert menu
+            dessertMenu.Add(new MenuItem("Gelato", "Non diary gelato of the day", true, 4.50));
+            dessertMenu.Add(new MenuItem("Snow ice campur", "Shaved ice with beans and fruits", true, 4.50));
+
+            // Add dessert to diner menu
+            dinerMenu.Add(dessertMenu);
+
+            Waitress waitress = new Waitress(allMenus);
+            waitress.PrintVegetarianMenu();
+
+            Console.ReadKey();
+        }
+    }
+}
