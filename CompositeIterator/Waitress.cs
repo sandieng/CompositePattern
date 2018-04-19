83e3dd23c0fca88b05f8687d78a22a09d0c9fe63
sandieng <sandi1_eng@yahoo.com>
2018-04-20 09:26:17 +1000

    Add files via upload

---
+++ CompositeIterator/Waitress.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CompositeIterator
+{
+    public class Waitress
+    {
+        private readonly MenuComponent _allMenus;
+
+        public Waitress(MenuComponent allMenus)
+        {
+            _allMenus = allMenus;
+        }
+
+        public void Print()
+        {
+            _allMenus.Print();
+        }
+
+        public void PrintVegetarianMenu()
+        {
+            Console.WriteLine("VEGATARIAN MENU");
+
+            foreach (MenuComponent menuComponent in _allMenus)
+            {
+                try
+                {
+                    if (menuComponent.IsVegetarian)
+                    {
+                        menuComponent.Print();
+                        Console.Write("\n");
+                    }
+                }
+                catch (NotSupportedException)
+                {
+                    Console.WriteLine("Operation not supported.");
+                }
+            }
+        }
+    }
+}
